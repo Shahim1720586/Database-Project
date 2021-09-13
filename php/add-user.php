1a6274ef4bbaf19fc94fce98976b02a9820f93ac
Shahim1720586 <1720586@iub.edu.bd>
2021-09-13 15:33:22 +0600

    project

---
+++ php/add-user.php
@@ -0,0 +1,22 @@
+<?php
+    require 'mysql.php';
+    // geting post requests
+    $id = $_POST['id'];
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $prog = $_POST['prog'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = strtolower($_POST['role']);
+    
+    $store = "INSERT INTO user (id, first_name, last_name, program_id, email, password, role) VALUES
+                ('$id', '$fName', '$lName', '$prog', '$email', '$password' , '$role')";
+
+    if($mysql->query($store)){
+        header("Location: ../admin/add-user.php");
+    }else{
+        header("Location: ../admin/add-user.php");
+    }
+
+    
+?>
---
+++ php/mysql.php
@@ -0,0 +1,7 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $key = '';
+    $db = 'spms-g6';
+    $mysql = mysqli_connect($host, $user, $key, $db);
+?>
